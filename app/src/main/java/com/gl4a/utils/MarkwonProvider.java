8b1ac3ae18680736db0d1eab057bd90138fdc7a8
OctoLab <octolab@secninjaz.com>
2026-07-27 10:23:28 -0400

    fix(theme): TextInputLayout hint colour; remove Markdown link underlines (squash from fix/98-hyperlinks)
    Closes #98.

--- app/src/main/java/com/gl4a/utils/MarkwonProvider.java
+++ app/src/main/java/com/gl4a/utils/MarkwonProvider.java
@@ -1,12 +1,24 @@
 package com.gl4a.utils;
 
 import android.content.Context;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+import android.view.View;
 
+import androidx.annotation.NonNull;
+
+import io.noties.markwon.AbstractMarkwonPlugin;
 import io.noties.markwon.Markwon;
+import io.noties.markwon.MarkwonSpansFactory;
+import io.noties.markwon.core.CoreProps;
 import io.noties.markwon.ext.strikethrough.StrikethroughPlugin;
 import io.noties.markwon.ext.tables.TablePlugin;
 import io.noties.markwon.ext.tasklist.TaskListPlugin;
 import io.noties.markwon.linkify.LinkifyPlugin;
+
+import com.gl4a.utils.IntentUtils;
+
+import org.commonmark.node.Link;
 
 public final class MarkwonProvider {
 
@@ -23,6 +35,30 @@ public final class MarkwonProvider {
                             .usePlugin(TablePlugin.create(context))
                             .usePlugin(TaskListPlugin.create(context))
                             .usePlugin(LinkifyPlugin.create())
+                            .usePlugin(new AbstractMarkwonPlugin() {
+                                @Override
+                                public void configureSpansFactory(
+                                        @NonNull MarkwonSpansFactory.Builder builder) {
+                                    // Replace default link span with one that has no underline
+                                    builder.setFactory(Link.class, (config, props) -> {
+                                        final String url = CoreProps.LINK_DESTINATION.require(props);
+                                        return new ClickableSpan() {
+                                            @Override
+                                            public void onClick(@NonNull View widget) {
+                                                IntentUtils.openLinkInternallyOrExternally(
+                                                        (androidx.fragment.app.FragmentActivity)
+                                                                widget.getContext(),
+                                                        android.net.Uri.parse(url));
+                                            }
+                                            @Override
+                                            public void updateDrawState(@NonNull TextPaint ds) {
+                                                ds.setColor(ds.linkColor);
+                                                ds.setUnderlineText(false);
+                                            }
+                                        };
+                                    });
+                                }
+                            })
                             .build();
                 }
             }
